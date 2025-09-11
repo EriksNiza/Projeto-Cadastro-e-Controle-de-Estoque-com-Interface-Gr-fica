fc5acc5d3344a536388bcde4eb32f9a42d2ef27a
EriksNiza <eriksniza7@gmail.com>
2025-09-11 08:45:51 -0300

    Update app.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import streamlit as st
 
-
-st.title('Título')
-
-st.text('Esta página apresenta o desenvolvimento de um pequeno sistema de controle de estoque voltado para uma cafeteria, simulando uma situação real de mercado. A proposta do projeto é criar uma ferramenta simples e funcional, que possa ser utilizada por pequenos comércios para gerenciar produtos e registrar vendas de forma prática e acessível.')
-
 with st.sidebar:
-    with st.sidebar:
-        add_radio = st.radio(
-        "Selecione as opções para escolher a página",
-        ("Página 1","Página 2")
+    st.title('Menu')
+    pagina = st.selectbox(
+        'Selecione uma das páginas',
+        ['Cadastro de produtos', 'Registro de vendas']
     )
+
+if pagina == 'Cadastro de produtos':
+    st.header("Cadastro de Produtos")
+
+    with st.expander('Registre seus produtos'):
+        nome_prod = st.text_input('Insira no nome do produto: ')
+        preco_prod = st.number_input('Insira o preço do produto: ')
+        estoque_prod = st.slider('Insira a quantidade em estoque: ', 0, 200)
+        enviar_prod = st.button('Enviar')
+
+        if enviar_prod:
+            st.success(f"Produto '{nome_prod}' cadastrado com sucesso!")
         
+    with st.expander('Visualize seus produtos'):
+        st.title('Oi')
+
+elif pagina == 'Registro de vendas':
+
+    st.header("Registro de Vendas")
+    st.expander('')
