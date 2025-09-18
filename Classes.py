7a4e0ca65c383cbad33bd8de43a58fa3981d557d
EriksNiza <eriksniza7@gmail.com>
2025-09-17 23:16:09 -0300

    Update Classes.py
    Nova classe Cliente com a junção da Api

--- Classes.py
+++ Classes.py
@@ -1,4 +1,35 @@
 from datetime import datetime
+import requests
+
+class Cliente:
+    def __init__(self, cpf, nome, data_nasc, cep, telefone):
+        self.cpf = str(cpf)
+        self.nome = nome.strip().title()
+        self.data_nasc = data_nasc
+        self.telefone = telefone
+        self.endereco = self.buscar_endereco(cep)
+
+    def buscar_endereco(self, cep):
+        url = f"https://brasilapi.com.br/api/cep/v1/{cep}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            dados = response.json()
+            endereco_formatado = (
+                f"{dados.get('street', '')}, {dados.get('neighborhood', '')}, "
+                f"{dados.get('city', '')} - {dados.get('state', '')}"
+            )
+            return endereco_formatado
+        else:
+            return "Endereço não encontrado"
+
+    def resumo(self):
+        return (f"CPF: {self.cpf} | Nome: {self.nome} | "
+                f"Nascimento: {self.data_nasc} | Telefone: {self.telefone} | "
+                f"Endereço: {self.endereco}")
+
+    def salvar_em_arquivo(self, arquivo="clientes.txt"):
+        with open(arquivo, "a", encoding="utf-8") as f:
+            f.write(f"{self.cpf};{self.nome};{self.data_nasc};{self.telefone};{self.endereco}\n")
 
 class Produtos:
 
@@ -36,7 +67,7 @@ class Produtos:
     
 
 class Vendas:
-    def __init__(self, produto, qtd, forma_pagamento=None, data=None):
+    def __init__(self, produto, qtd,cliente, forma_pagamento=None, data=None, ):
         if int(qtd) <= 0:
             raise ValueError("A quantidade da venda deve ser positiva.")
         if int(qtd) > int(produto.qtd_estoque):
@@ -47,11 +78,11 @@ class Vendas:
         self.qtd = qtd
         self.valorTotal = qtd * produto.preco
         self.forma_pagamento = forma_pagamento or "Dinheiro"
-
+        self.cliente=cliente
     def resumo(self):
             return (f"{self.data} | Produto: {self.produto} | "
                 f"Qtd: {self.qtd} | Total: R${self.valorTotal:.2f} | "
-                f" Pagamento: {self.forma_pagamento}")
+                f" Pagamento: {self.forma_pagamento} | Cliente: {self.cliente}")
     
     def aplicar_desconto(self, percentual):
         if percentual <= 0 or percentual >= 100:
